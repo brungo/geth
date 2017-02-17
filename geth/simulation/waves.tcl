160d1ce6c8b093e9d1d9dd89530bd576ed592b71
Bruno <valinoti@inti.gob.ar>
2017-02-17 16:12:40 -0300

    waves.tcl --> Signals grouped for collapsing

--- geth/simulation/waves.tcl
+++ geth/simulation/waves.tcl
@@ -5,41 +5,44 @@ wave add /testbench/dut/trimac_locallink/trimac_block/refclk
 wave add /testbench/dut/trimac_locallink/trimac_block/rx_enable
 
 divider add "GMII"
-wave add /testbench/gtx_clk
-wave add /testbench/gmii_tx_clk
-wave add /testbench/gmii_txd -radix hex
-wave add /testbench/gmii_tx_en
-wave add /testbench/gmii_tx_er
-wave add /testbench/gmii_rx_clk
-wave add /testbench/gmii_rxd -radix hex
-wave add /testbench/gmii_rx_dv
-wave add /testbench/gmii_rx_er
-wave add /testbench/gmii_crs
-wave add /testbench/gmii_col
-wave add /testbench/mii_tx_clk
+set GMII [group add "GMII"]
+wave add -into $GMII /testbench/gtx_clk
+wave add -into $GMII /testbench/gmii_tx_clk
+wave add -into $GMII /testbench/gmii_txd -radix hex
+wave add -into $GMII /testbench/gmii_tx_en
+wave add -into $GMII /testbench/gmii_tx_er
+wave add -into $GMII /testbench/gmii_rx_clk
+wave add -into $GMII /testbench/gmii_rxd -radix hex
+wave add -into $GMII /testbench/gmii_rx_dv
+wave add -into $GMII /testbench/gmii_rx_er
+wave add -into $GMII /testbench/gmii_crs
+wave add -into $GMII /testbench/gmii_col
+wave add -into $GMII /testbench/mii_tx_clk
 
 divider add "Tx_if"
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_clk
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_enable
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_ifg_delay
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_statistics_vector
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_statistics_valid
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_data -radix hex
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_data_valid
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_ack
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_underrun
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_collision
-wave add /testbench/dut/trimac_locallink/trimac_block/tx_retransmit
+set Tx_if [group add "Tx_if"]
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_clk
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_enable
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_ifg_delay
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_statistics_vector
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_statistics_valid
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_data -radix hex
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_data_valid
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_ack
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_underrun
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_collision
+wave add -into $Tx_if /testbench/dut/trimac_locallink/trimac_block/tx_retransmit
 
 divider add "Rx_if"
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_clk
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_enable
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_statistics_vector
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_statistics_valid
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_data -radix hex
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_data_valid
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_good_frame
-wave add /testbench/dut/trimac_locallink/trimac_block/rx_bad_frame
+set Rx_if [group add "Rx_if"]
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_clk
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_enable
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_statistics_vector
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_statistics_valid
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_data -radix hex
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_data_valid
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_good_frame
+wave add -into $Rx_if /testbench/dut/trimac_locallink/trimac_block/rx_bad_frame
 
 #divider add "MAC control IF"
 #wave add /testbench/dut/trimac_locallink/trimac_block/pause_req
@@ -47,6 +50,7 @@ wave add /testbench/dut/trimac_locallink/trimac_block/rx_bad_frame
 #wave add /testbench/dut/client_side_asm/rx_ll_data_in -radix hex
 #wave add /testbench/dut/client_side_asm/rx_ll_data_out -radix hex
 
+divider add "FIFO_Control"
 set FIFO_Control [group add "FIFO_Control"]
 wave add -into $FIFO_Control /testbench/dut/trimac_locallink/client_side_FIFO/tx_ll_clock
 wave add -into $FIFO_Control /testbench/dut/trimac_locallink/client_side_FIFO/tx_ll_reset
